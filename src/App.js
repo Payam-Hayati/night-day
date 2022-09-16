816352c8e3e295ff83c075200f3a394772a088f2
Your Name <payam.hayati@yahoo.com>
2022-09-16 18:11:56 +0430

    ADD: "night day"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+//import Navbar from "./components/Navbar";
+import styled from "styled-components";
+import Switch from "react-switch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[1000px] mx-auto pt-7 pl-20 pr-20">
+      <Container>
+
+        <div className="nav">
+          <ul>
+            <li> خانه </li>
+            <li> قیمت ها </li>
+            <li> تیم </li>
+            <li> تماس </li>
+          </ul>
+        </div>
+        <div className="swith">
+          {/* <Switch /> */}  switch
+        </div>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  background-color: white;
+  border-radius: 10px;
+  justify-content: space-between;
+  direction: rtl;
+  /* flex-wrap: wrap; */
+  flex:1;
+  flex-grow:1;
+
+  ul {
+    list-style-type: none;
+    li {
+      display: inline-block;
+      padding-right: 20px;
+      font-weight: bold;
+      cursor: pointer;
+    }
+  }
+`;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
+
+
+
+const Container = styled.div``
